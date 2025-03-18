89c0d3e744df1f379577e1a2d7c041abdc6b0d14
Alex McColm <mccolmalex@gmail.com>
2025-03-18 16:07:44 -0700

    Fill in question from a json file

--- action.js
+++ action.js
@@ -1,4 +1,9 @@
-const helloWorld = () => {
-    console.log('Hello, World!');
-    alert('Hello, World!');
-}
+const question = {
+    prompt: 'What is the first colour in the rainbow?',
+    title: 'Question 1'
+};
+
+const extractQuestionData = () => {
+    document.getElementById('questionprompt').innerText = question.prompt;
+    document.getElementById('questiontitle').innerText = question.title;
+};
