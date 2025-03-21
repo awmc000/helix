51d34889354d6849e95e5e96a664d195f0daa57c
4treess <145797309+4treess@users.noreply.github.com>
2025-03-20 23:13:40 -0700

    Update Statements.sql
    Its kinda funny how complicated these statements get that quickly

--- Statements.sql
+++ Statements.sql
@@ -56,7 +56,68 @@ SELECT questionID, prompt, durationMinutes, durationSeconds FROM Question WHERE 
 SELECT optionNumber, optionDescription, scoreValue FROM AnswerKey WHERE questionID = %s;
 
 
--- 
+-- Get the number of responses
 
+SELECT COUNT(*) FROM Answers WHERE questionID = %s;
 
+-- Get the sum of the scores per question
 
+SELECT SUM(scoreValue) FROM AnswerKey NATURAL JOIN Answers WHERE questionID = %s;
+
+-- Question with fewest correct answers
+
+WITH correctAnswers AS (SELECT attemptID FROM Answers NATURAL JOIN AnswerKey WHERE scoreValue > 0 GROUP BY attemptID) SELECT MIN(correct) AS minCorrect, prompt FROM (SELECT prompt, COUNT(DISTINCT correctAnswers.attemptID) AS correct FROM Answers NATURAL JOIN Question JOIN correctAnswers ON Answers.attemptID = correctAnswers.attemptID JOIN Quiz ON Quiz.quizID = Question.quizID WHERE Quiz.quizID = %s GROUP BY prompt) AS counts GROUP BY prompt;
+
+-- Question with Most correct answers
+
+WITH correctAnswers AS (SELECT attemptID FROM Answers NATURAL JOIN AnswerKey WHERE scoreValue > 0 GROUP BY attemptID) SELECT MAX(correct) AS maxCorrect, prompt FROM (SELECT prompt, COUNT(DISTINCT correctAnswers.attemptID) AS correct FROM Answers NATURAL JOIN Question JOIN correctAnswers ON Answers.attemptID = correctAnswers.attemptID JOIN Quiz ON Quiz.quizID = Question.quizID WHERE Quiz.quizID = %s GROUP BY prompt) AS counts GROUP BY prompt;
+
+-- Question with the most Homogonous Answers
+
+-- ChatGPT's Solution (Modified So it works in our db)
+
+WITH correctAnswers AS (
+    SELECT attemptID
+    FROM Answers
+    NATURAL JOIN AnswerKey
+    WHERE scoreValue > 0
+    GROUP BY attemptID
+),
+attemptCount AS (
+    SELECT prompt, COUNT(DISTINCT correctAnswers.attemptID) AS correct
+    FROM Answers NATURAL JOIN Question
+    JOIN correctAnswers ON Answers.attemptID = correctAnswers.attemptID
+    Join Quiz ON Quiz.quizID = Question.quizID
+    WHERE Quiz.quizID = %s
+    GROUP BY prompt
+)
+SELECT prompt, VARIANCE(correct) AS variance
+FROM attemptCount
+GROUP BY prompt
+ORDER BY variance ASC
+LIMIT 1;
+
+-- Question with the most Heterogenous Answers
+
+-- ChatGPT's Solution (Modified So it works in our db)
+
+WITH correctAnswers AS (
+    SELECT attemptID
+    FROM Answers
+    NATURAL JOIN AnswerKey
+    WHERE scoreValue > 0
+    GROUP BY attemptID
+),
+attemptCount AS (
+    SELECT prompt, COUNT(DISTINCT correctAnswers.attemptID) AS correct
+    FROM Answers NATURAL JOIN Question
+    JOIN correctAnswers ON Answers.attemptID = correctAnswers.attemptID
+    Join Quiz ON Quiz.quizID = Question.quizID
+    WHERE Quiz.quizID = %s
+    GROUP BY prompt
+)
+SELECT prompt, VARIANCE(correct) AS variance
+FROM attemptCount
+GROUP BY prompt
+ORDER BY variance DESC
+LIMIT 1;
