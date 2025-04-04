aec5d94ba1ea5cbde0edfbae485a8e64d4199e93
glenbeatty <glenbeatty@gmail.com>
2025-04-03 19:06:37 -0700

    more debug, error handling in dbApplication.py

--- tests/dbApplicationUT.py
+++ tests/dbApplicationUT.py
@@ -1,10 +1,15 @@
+# File name: dbApplicationUT.py
+# Last edited by Glen on April 3rd, 2025
+
+# Brief: Contains a test suite for functions in dbApplication.py
+
+'''Description:
+An example of a unit test for dbApplications.py. While it is common for a unit test suite to corrospond to a particular project file, it is not necessarily the case. An individual unit test file is just meant to be a repeatable set of tests to validate that the project still works as intended. 
+'''
+
 from unitTestFunctions import * 
 
-if __name__ == "__main__":
-    
-    correct_username = os.getenv("DBUSER")
-    correct_password = os.getenv("DBPASS")
-
+def ctdTests(correctUsername, correctPassword):
     ctdUnitTest = connectToDatabase_UT("connectToDatabase", "DbApplication.py")
     ctdUnitTest.addTestCase("Correct Credentials", correct_username, correct_password, True)
     ctdUnitTest.addTestCase("Reversed Credentials", correct_password, correct_username, False)
@@ -15,6 +20,7 @@ if __name__ == "__main__":
     ctdUnitTest.addTestCase("Wrong username, bad pw format", "fakeusername", 11, False)
     ctdResult = ctdUnitTest.runTestCases(verbose=True)
 
+def dfdTests(correctUsername, correctPassword):
     database = None
     dfdUnitTest = disconnectFromDatabase_UT("disconnectFromDatabase", "DbApplication.py")
     dfdUnitTest.addTestCase("None-type database", database, False)
@@ -22,3 +28,42 @@ if __name__ == "__main__":
     dfdUnitTest.addTestCase("Correct Database Object Provided", database2, True)
     dfdUnitTest.addTestCase("Database was not connected", database2, False)
     dfdResult = dfdUnitTest.runTestCases(verbose=True)
+
+if __name__ == "__main__":
+    
+    correct_username = os.getenv("DBUSER")
+    correct_password = os.getenv("DBPASS")
+    #ctdTests(correct_username, correct_password)
+    #dfdTests(correct_username, correct_password)
+
+    database = None
+    database = db.connectToDatabase(correct_username,correct_password)
+    if database.is_connected():
+        print("Database Is Connected.")
+    else:
+        print("Database Is Not Connected.")
+
+    author = Author(username="gbeatty", name="Glen Beatty", description="Pretty Cool Guy", emailAddress="fake@email.com")
+    author2 = Author(username="gbeattddfasdy", name="Glen Beatty", description="Pretty Cool Guy", emailAddress="fake@email.com")
+    try: 
+        db.deleteAuthor(author, database)
+        print("Hello World")
+    except Exception as e:
+        print(e)
+
+    try: 
+        db.deleteAuthor(author, database)
+        print("Hello World")
+
+    except Exception as e:
+        print(e)
+
+    print("Does gbeatty exist?: ", db.doesAuthorExist("gbeatty", database))
+    db.createAuthor(author, database)
+    
+    print("Does gbeatty exist now?: ", db.doesAuthorExist("gbeatty", database))
+    print("Results of username lookup:", db.getAuthorByUsername("gbeatty", database))
+    #print(*db.getAuthorList(database), sep="\n")
+    #print("Delete #1: ",db.deleteAuthor(author, database))
+
+    #print("Delete #2: ", db.deleteAuthor(author2,database))
--- tests/unitTestFunctions.py
+++ tests/unitTestFunctions.py
@@ -1,17 +1,41 @@
+# File name: unitTestFunctions.py
+# Last edited by Glen on April 3rd, 2025
+
+# Brief: Contains classes necessary to create suites of unit tests. 
+
+''' Description:
+This file contains the classes and functions necessary to create and run suites of unit tests. This file is not meant to be called directly and does not contain a main method. 
+
+Classes and methods will be called in separate unit test files. For example, a set of tests meant to validate the functionality of dbApplications.py is contained in dbApplicationUT.py. The goal of this structure is to allow unit tests to be easily compartmentalized. A series of tests could be used to test a particular file or component of the application. A larger test could be constructed to validate all functionality on the server-side and could be run before major updates are pushed. 
+
+A base unitTest class is defined and then extended upon for each unit test. Where possible, each derived class of unitTest corrosponds to a particular function or class that is being tested. 
+'''
+
+# Necessary to obtain system username/password and to include files from parent directory.
 import sys
 import os 
-
 sys.path.append("..")
 
 import dbApplication as db
-
+from models import *
+# Base unitTest class. Typically not used directly. 
 class unitTest:
+
+    # Called to initialize unitTest object. 
+    # Param testName is the name of the unit test.
+    # param file is the file containing the unit being tested (eg: dbApplication.py)
     def __init__(self, testName, file):
+
         self.testName = testName
         self.file = file
+
+        # Empty list to contain all test cases for a particular unit test.
         self.testCases = []
-        self.numTestCases = 0
+        self.numTestCases = 0 # Tracks how many cases there are. 
     
+    # Print function to allow for (hopefully) readable output.
+    # Param char is simply the character to be printed.
+    # Param qty is the number of times the char should be printed, which defaults to 100.
     def _lineBreak(self, char, qty = 100):
         line = ""
         for x in range(qty):
@@ -19,21 +43,48 @@ class unitTest:
         print(line)
 
 
+# ***** Begin unit tests for dbApplication.py *****
+
+# Unit test class for connectToDatabase function.
 class connectToDatabase_UT(unitTest):
+
+    # runTestCase function runs an individual test case, based on the provided paramaters. This method can be called directly to test an individual test case, but is also called to test all test cases by runTestCases method.
+
+    # Param caseName is the name of the test case (eg: "Bad password").
+    # Param dbUsername is the username to test.
+    # Param dbPassword is the password to test.
+    # Param expectedSuccessValue is the expected outcome of the test. For example, a correct username and password would be expected to return true.
+    # Param verbose specifies whether the detailed results of the test should be printed. If this argument is omitted or a false value specified, only brief results will be printed. 
+
     def runTestCase(self, caseName, dbUsername, dbPassword, expectedSuccessValue, verbose = False):
         
+        # Verify that username and password are of the correct type (string).
         usernameIsValid = type(dbUsername) is str
         passwordIsValid = type(dbPassword) is str
+
+        #Initialize database to None type and create empty list to store errors. 
         database = None
         errors = []
+
+        # Try to connect to the database.
         try:
             database = db.connectToDatabase(dbUsername, dbPassword)
+
+        # If dbApplication.py throws an exception, store it.
         except Exception as e:
             errors.append(e)
+
+        # Boolean to store whether the database connected successfully or not. 
         databaseConnectSuccess = database!=None
+
+        # If DB connected, we need to disconnect as well.
         if(databaseConnectSuccess):
             db.disconnectFromDatabase(database)
+        
+        # Determine the total success value of this test case (only true if all tests pass)
         unitTestSuccess = usernameIsValid and passwordIsValid and databaseConnectSuccess
+
+        # Print the results of the test case
         self._lineBreak('*')
         print("Testing case:", caseName,) 
         if(unitTestSuccess == expectedSuccessValue):
@@ -49,19 +100,24 @@ class connectToDatabase_UT(unitTest):
                 print("\tRaised Errors:")
                 for e in errors:
                     print("\t-", e)
+
+        # Return true if the test case result matched the expected result. This still returns true if a test was expected to fail and did fail. Similarly, if a test was expected to fail and passed false is returned.
         return unitTestSuccess == expectedSuccessValue
 
-            
+    # addTestCase function adds a testCase to be tested later using runTestCases function. 
+    # Param name is a name assigned to a given test case, and is used to identify the result of each test case.
     def addTestCase(self, name, userName, password, expectedSuccess):
         self.numTestCases += 1
         self.testCases.append([self.numTestCases,name,userName,password,expectedSuccess,])
 
-
+    # Helper function, primarily used in development of test case suites. Is not called when running individual or series of test cases.
     def printTestCases(self):
         self._lineBreak("^")
         print("Printing all test cases for unit test:", self.testName)
         print(*self.testCases, sep="\n")
 
+    # Function used to call all test cases previously added using the addTestCase method.
+    # Param verbose specifices whether brief or long-form results should be printed.
     def runTestCases(self, verbose = False):
         self._lineBreak('=')
         print("Running", self.numTestCases, "test cases for unit test:",self.testName)
